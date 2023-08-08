43a7439322f658f799bd014b81a4cdbb890eebbd
Jade-Dragon88 <60228043+Jade-Dragon88@users.noreply.github.com>
2023-08-08 12:29:52 +0000

    Установил react-helmet

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -2,15 +2,15 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import './singleComicPage.scss';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
+// import Spinner from '../spinner/Spinner';
+// import ErrorMessage from '../errorMessage/ErrorMessage';
 import useMarvelService from '../../services/MarvelService';
 import setContent from '../../utils/setContent';
 
 
 const SingleComicPage = () => {
     const {comicId} = useParams();
-    const {comic, setComic} = useState();
+    const {comic, setComic} = useState(null);
     const { loading, error, getComic, clearError,process,setProcess } = useMarvelService();
     
     useEffect(() => { updateComic() }, [comicId])
@@ -22,27 +22,27 @@ const SingleComicPage = () => {
       clearError();
       getComic(comicId)
         .then(onComicLoaded)
-        // .then(()=>setProcess('confirmed'));
+        .then(()=>setProcess('confirmed'));
     }
 
-    const spinner = loading ? <Spinner /> : null;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const content = !(!comic || loading || error) ? <View comic={comic} /> : null;
+    // const spinner = loading ? <Spinner /> : null;
+    // const errorMessage = error ? <ErrorMessage /> : null;
+    // const content = !(!comic || loading || error) ? <View comic={comic} /> : null;
 
   
     return(
       <>
-        {errorMessage}
+        {/* {errorMessage}
         {spinner}
-        {content}
-        {/* {setContent(process,View,comic)} */}
+        {content} */}
+        {setContent(process,View,comic)}
       </>
     )
     
 }
 
-const View =({comic})=>{
-  const {title, description, pageCount, thumbnail, language, price} = comic; 
+const View =({data})=>{
+  const {title, description, pageCount, thumbnail, language, price} = data; 
   return (
         <div className="single-comic">
             <img src={thumbnail} alt={title} className="single-comic__img"/>
